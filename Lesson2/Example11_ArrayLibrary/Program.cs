d4e491d1411c89ae02021ed64cff8fe3cfdd6758
Olga Zhukova <olga.v.zhukoff@gmail.com>
2022-10-04 16:20:06 +0300

    Example11 Array Library added

---
+++ Lesson2/Example11_ArrayLibrary/Program.cs
@@ -0,0 +1,30 @@
+﻿// мы определили массив из 10 элементов, заполнили массив случайными
+// числами (метод FillArray), а метод PrintArray нам его распечатал.
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        // index = index + 1;
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
